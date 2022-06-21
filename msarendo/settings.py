6d941ea2e23986582bf8d70db7bb1ff894d9e58a
vanta blanta <vantablanta@gmail.com>
2022-06-21 15:58:23 +0300

    postgres db added

--- msarendo/settings.py
+++ msarendo/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'msarendo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+            'NAME': str(os.getenv("NAME")),
+            'USER': str(os.getenv("USER")),
+            'PASSWORD': str(os.getenv("PASSWORD")),
+            'HOST': str(os.getenv("HOST")),
+            'PORT':os.getenv("PORT")
     }
 }
 
