880de2c73191ec84223b3ba51a720435e2b89619
vanta blanta <vantablanta@gmail.com>
2022-06-21 16:25:24 +0300

    login and register pages added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,10 @@ from .import views
 
 
 urlpatterns = [
-path('', views.home, name='home')
+path('login/', views.login_user, name='login'),
+path('register/', views.register_user, name='register'),
+
+
+path('', views.home, name='home'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_user(request):
+    ctx = {}
+    return render(request, 'app/login.html')
+
+def register_user(request):
+    ctx = {}
+    return render(request, 'app/register.html')
+
 def home(request):
     return render(request, 'app/index.html' )
